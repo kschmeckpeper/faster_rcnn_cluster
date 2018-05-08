0b204195bf28a0a0bffa8b275abab558373ccc4e
Karl Schmeckpeper <karl.schmeckpeper@gmail.com>
2018-05-08 13:11:00 -0400

    Added reading classes from file

--- lib/datasets/pascal_voc.py
+++ lib/datasets/pascal_voc.py
@@ -27,12 +27,24 @@ class pascal_voc(imdb):
         self._devkit_path = self._get_default_path() if devkit_path is None \
                             else devkit_path
         self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
+
+
         self._classes = ('__background__', # always index 0
                          'aeroplane', 'bicycle', 'bird', 'boat',
                          'bottle', 'bus', 'car', 'cat', 'chair',
                          'cow', 'diningtable', 'dog', 'horse',
                          'motorbike', 'person', 'pottedplant',
                          'sheep', 'sofa', 'train', 'tvmonitor')
+
+
+        if os.path.isfile(os.path.join(self._data_path, "classes.txt")):
+            with open(os.path.join(self._data_path, "classes.txt")) as class_file:
+                for index, class_name in enumerate(class_file):
+                    self._classes[index + 1] = class_name
+
+            print self._classes
+        exit()
+
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
